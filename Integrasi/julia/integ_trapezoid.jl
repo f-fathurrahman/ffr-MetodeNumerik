4eff91f4323c4991abac0269d7239ddc76f05e01
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-12-28 22:06:48 +0700

    Add integ_trapezoid.jl, f is given as an array

--- Integrasi/julia/integ_trapezoid.jl
+++ Integrasi/julia/integ_trapezoid.jl
@@ -1,3 +1,9 @@
+# Numerical integration using trapezoidal rule
+#
+# f : function to be integrated, defined as a \texttt{Function}
+# a : lower bound
+# b : upper bound
+# B : number
 function integ_trapezoid( f::Function, a, b, N )
     x = a
     h = (b-a)/N
@@ -9,4 +15,18 @@ function integ_trapezoid( f::Function, a, b, N )
     end
     s = s + f(b)
     return (b-a)*s/(2*N)
+end
+
+
+function integ_trapezoid( f::Array{Float64,1}, a, b, N )
+    if length(f) != N + 1
+        Base.AssertionError("Length of f is not equal to N + 1")
+    end
+    h = (b-a)/(N+1)
+    s = f[1] + f[N+1]
+    #
+    for i = 2:N
+        s = s + 2*f[i]
+    end
+    return h*s
 end
