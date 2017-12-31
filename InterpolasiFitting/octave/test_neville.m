407b566a51de3b15b6cb2a0e71cd20cb14ecacbe
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-12-31 17:17:54 +0700

    Add InterpolasiFitting, Newton and Neville interpolation

---
+++ InterpolasiFitting/octave/test_neville.m
@@ -0,0 +1,8 @@
+% Example 3.4 (Neville's interpolation)
+xData = [0.15; 2.3; 3.15; 4.85; 6.25; 7.95];
+yData = [4.79867; 4.49013; 4.22430; 3.47313; 2.66674; 1.51909];
+for x = 0:0.5:8
+  y = neville(xData, yData, x);
+  yExact = 4.8*cos(pi*x/20);
+  fprintf('%10.5f %10.5f %10.5f    %10.5e\n', x, y, yExact, abs(y-yExact) );
+end
