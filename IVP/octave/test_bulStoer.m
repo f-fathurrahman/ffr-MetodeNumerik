4388746ded90dd2f87db1dad3e140a1f18637bc2
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-01-05 10:56:42 +0700

    Add bulStoer and related files, need to test

---
+++ IVP/octave/test_bulStoer.m
@@ -0,0 +1,8 @@
+% Example 7.11 (Bulirsch-Stoer integration)
+[xSol,ySol] = bulStoer(@fex7_11,0,[0 0],10,0.5);
+printSol(xSol,ySol,1)
+
+plot(xSol,ySol(:,2),'k-o')
+grid on
+xlabel('Time (s)')
+ylabel('Current (A)')
