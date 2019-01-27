1386c3e0284c34db4a6c92cb62f848f475d325f7
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-01-27 20:47:49 +0700

    Test chapra_ch5

--- AkarPersamaan/python3/bisection.py
+++ AkarPersamaan/python3/bisection.py
@@ -1,7 +1,7 @@
 import numpy as np
 from math import log, ceil
 
-def bisection(f, x1, x2, TOL=1.0e-9, verbose=False):
+def bisection(f, x1, x2, TOL=1.0e-9, verbose=False, Niter=None):
 
     if verbose:
         print("Searching root with bisection method:")
@@ -18,12 +18,22 @@ def bisection(f, x1, x2, TOL=1.0e-9, verbose=False):
     if np.sign(f1) == np.sign(f2):
         raise RuntimeError("Root is not bracketed")
 
-    Niter = int(ceil( log(abs(x2-x1)/TOL) )/ log(2.0) )
+    if Niter == None:
+        Niter = int(ceil( log(abs(x2-x1)/TOL) )/ log(2.0) )
+    
+    if verbose:
+        print("Bisection will iterate upto %d iterations" % (Niter))
 
     if verbose:
         print("Niter = ", Niter)
 
+    # For the purpose of calculating relative error
+    x3 = 0.0
+    x3_old = 0.0
+
     for i in range(Niter):
+
+        x3_old = x3
         x3 = 0.5*(x1 + x2)
         f3 = f(x3)
 
@@ -43,4 +53,6 @@ def bisection(f, x1, x2, TOL=1.0e-9, verbose=False):
             x2 = x3
             f2 = f3
 
-    return 0.5*(x1 + x2)
+    # return the result
+    return x3, abs(x3 - x3_old)
+
--- AkarPersamaan/python3/test_root_02.py
+++ AkarPersamaan/python3/test_root_02.py
@@ -1,4 +1,5 @@
 from bisection import *
+import chapra_ch05
 
 def f(x):
     return x**3 - 10*x**2 + 5.0
@@ -6,5 +7,8 @@ def f(x):
 x1 = 0.0
 x2 = 1.0
 
-x = bisection(f, x1, x2, TOL=1e-4)
+x, _ = bisection(f, x1, x2, TOL=1e-4)
+print("x = {:6.4f}".format(x))
+
+x, _, _ = chapra_ch05.bisect(f, x1, x2, TOL=1e-4)
 print("x = {:6.4f}".format(x))
