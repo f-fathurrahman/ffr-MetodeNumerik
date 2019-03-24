74fcce51e547e8c1c97f58b19aa363ce94544171
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-03-24 20:07:22 +0700

    Implement sinusoid_fit, a special case of fourier fit, also for non-equispaced data

--- InterpolasiFitting/python3/fourier_fit.py
+++ InterpolasiFitting/python3/fourier_fit.py
@@ -8,7 +8,7 @@ def fourier_fit(x, y, m=1):
 
     assert N == len(y)
 
-    assert N > 2*m + 1
+    assert N > (2*m + 1)
 
     s = np.zeros(N)
     for i in range(N):
@@ -24,8 +24,8 @@ def fourier_fit(x, y, m=1):
             A[i-1] = A[i-1] + y[j]*cos(i*s[j])
             B[i-1] = B[i-1] + y[j]*sin(i*s[j])
 
-    A = 2*A/N
-    B = 2*B/N
+    A = 2.0*A/N
+    B = 2.0*B/N
 
     return A0, A, B
 
@@ -39,10 +39,69 @@ def fourier_fit_eval(A0, A, B, x, xo):
     xmax = np.max(x)
     s = (x - xmin)*(N-1)*2*pi/(N*(xmax-xmin))
     so = (xo - xmin)*(N-1)*2*pi/(N*(xmax-xmin))
-    print("so = ", so)
     y = A0
     for i in range(1,m+1):
         y = y + A[i-1]*cos(i*so) + B[i-1]*cos(i*so)
     return y
 
+
+def sinusoid_fit(x, y, equispaced=True, T=0.0):
+    N = len(x)
+
+    assert N == len(y)
+
+    if equispaced:
+        deltax = x[1] - x[0]
+        T = (N-1)*deltax
+        print("deltax = ", deltax)
+    else:
+        if T <= 0:
+            raise RuntimeError("Perlu T > 0")
+    
+    omega = 2*pi/T
+
+    if equispaced:
+
+        A0 = np.sum(y)/N
+
+        A1 = 0.0
+        B1 = 0.0
+        for i in range(N):
+            A1 = A1 + y[i]*cos(omega*x[i])
+            B1 = B1 + y[i]*sin(omega*x[i])
+        A1 = 2.0*A1/N
+        B1 = 2.0*B1/N
+
+        return A0, A1, B1, omega
+
+    else:
+
+        NormalMatrix = np.zeros((3,3))
+        RHSVector = np.zeros(3)
+
+        NormalMatrix[0,0] = N
+        NormalMatrix[1,1] = np.sum( np.cos(omega*x)**2 )
+        NormalMatrix[2,2] = np.sum( np.sin(omega*x)**2 )
+
+        NormalMatrix[0,1] = np.sum( np.cos(omega*x) )
+        NormalMatrix[1,0] = NormalMatrix[0,1]
+
+        NormalMatrix[0,2] = np.sum( np.sin(omega*x) )
+        NormalMatrix[2,0] = NormalMatrix[0,2]
+
+        NormalMatrix[2,1] = np.sum( np.cos(omega*x)*np.sin(omega*x) )
+        NormalMatrix[1,2] = NormalMatrix[2,1]
+
+        RHSVector[0] = np.sum(y)
+        RHSVector[1] = np.sum(y*np.cos(omega*x))
+        RHSVector[2] = np.sum(y*np.sin(omega*x))
+
+        sol = np.linalg.solve(NormalMatrix, RHSVector)
+
+        return sol[0], sol[1], sol[2], omega
+
+
+
+def sinusoid_fit_eval(A0, A1, B1, omega, x):
+    return A0 + A1*cos(omega*x) + B1*sin(omega*x)
 
