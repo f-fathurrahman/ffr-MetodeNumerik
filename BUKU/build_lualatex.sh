1def7ecd6c91712a96514dc77f29280522644e94
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-01-11 19:02:48 +0700

    Bab 1

--- BUKU/build_lualatex.sh
+++ BUKU/build_lualatex.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-#./do_pweave.sh Bisection.texw
-#./do_pweave.sh RegulaFalsi.texw
-./do_pweave.sh FixedPoint.texw
-
 lualatex --shell-escape BUKU_METODE_NUMERIK.tex
 lualatex --shell-escape BUKU_METODE_NUMERIK.tex
