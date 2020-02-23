9fcb9508b6a7fa15452fb02869c84377ad5229b1
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-02-23 12:52:35 +0700

    More printing test

--- 12Steps_NSE/python/test_sympy.py
+++ 12Steps_NSE/python/test_sympy.py
@@ -11,3 +11,10 @@ print(printing.latex(delta__y_l))
 
 u_i_np1 = symbols("u_i__n+1")
 print(printing.latex(u_i_np1))
+
+u_ip1_np1 = symbols("u_i+1__n+1")
+print(printing.latex(u_ip1_np1))
+
+u_ip1j_np1 = symbols("u_{i+1,j}__n+1")
+print(printing.latex(u_ip1j_np1))
+pprint(u_ip1j_np1)
