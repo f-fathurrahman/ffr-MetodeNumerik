3f4b881bbd8a214052262e2a8655deec40c247be
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-02-23 12:48:26 +0700

    SymPy latex printing

--- 12Steps_NSE/python/test_sympy.py
+++ 12Steps_NSE/python/test_sympy.py
@@ -3,3 +3,11 @@ from sympy import *
 #u_i_n = Symbol("u_i^{n}")
 u_i_n = Symbol("u_i^n")
 pprint(u_i_n)
+
+import sympy.printing as printing
+
+delta__y_l = symbols("Delta__y_l")
+print(printing.latex(delta__y_l))
+
+u_i_np1 = symbols("u_i__n+1")
+print(printing.latex(u_i_np1))
