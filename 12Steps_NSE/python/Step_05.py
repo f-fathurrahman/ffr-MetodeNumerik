f3b94e9226a642c657fa85cfba088b5ab4a1cdf6
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-02-23 13:13:42 +0700

    2d advection

---
+++ 12Steps_NSE/python/Step_05.py
@@ -0,0 +1,50 @@
+from mpl_toolkits.mplot3d import Axes3D
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+Nx = 81
+Ny = 81
+Nt = 100
+c = 1.0
+Δx = 2.0/(Nx-1)
+Δy = 2.0/(Ny-1)
+σ = 0.2
+Δt = σ*Δx
+
+x = np.linspace(0.0, 2.0, Nx)
+y = np.linspace(0.0, 2.0, Ny)
+
+u = np.ones( (Nx,Ny) )
+un = np.ones( (Nx,Ny) )
+
+# Initial conditions
+u[int(0.5/Δy):int(1.0/Δy + 1),int(0.5/Δx):int(1.0/Δx + 1)] = 2.0
+
+plt.clf()
+fig = plt.figure(figsize=(11,7))
+ax = fig.gca(projection="3d")
+X, Y = np.meshgrid(x,y)
+surf = ax.plot_surface(X, Y, u[:], cmap=cm.jet)
+plt.savefig("IMG_Step_05_0000.png", dpi=150)
+
+for n in range(Nt + 1):
+    #for n in range(2):
+    un = u.copy()
+    row, col = u.shape
+    for j in range(1, row):
+        for i in range(1, col):
+            u[j, i] = (un[j, i] - (c*Δt/Δx * (un[j, i] - un[j, i - 1])) - (c*Δt/Δy * (un[j, i] - un[j - 1, i])))
+            u[0, :] = 1.0
+            u[-1, :] = 1.0
+            u[:, 0] = 1.0
+            u[:, -1] = 1.0
+    fig.clf()
+    ax = fig.gca(projection="3d")
+    X, Y = np.meshgrid(x,y)
+    surf = ax.plot_surface(X, Y, u[:], cmap=cm.jet)
+    filename = "IMG_Step_05_{:04d}.png".format(n + 1)
+    fig.savefig(filename, dpi=150)
+    print("Done: ", n)
+
