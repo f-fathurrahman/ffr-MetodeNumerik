fac3bfb9cbd3a7248193397a34d35f587fce0bd2
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2020-03-24 20:58:55 +0700

    Draft

--- TF4062_KompRekLanjut/FEM/test_04.py
+++ TF4062_KompRekLanjut/FEM/test_04.py
@@ -9,7 +9,7 @@ init_printing()
 # xgrid, h: array (numeric)
 # L: numeric
 def build_shape_functions( x, xgrid, h ):
-    Npoints = len(xgrid)
+    Nnodes = len(xgrid)
     
     # First shape functions
     cond1 = (x >= xgrid[0]) & (x <= xgrid[1])
@@ -18,7 +18,7 @@ def build_shape_functions( x, xgrid, h ):
     
     shape_functions = [ N_first ]
     
-    for i in range(1,Npoints-1):
+    for i in range(1,Nnodes-1):
         #
         cond1 = (x >= xgrid[i-1]) & (x <= xgrid[i])
         f1 = ( x - xgrid[i-1])/h[i-1]
@@ -29,50 +29,42 @@ def build_shape_functions( x, xgrid, h ):
         #
         shape_functions.append(N_i)
     
-    cond1 = (x >= xgrid[Npoints-2]) & (x <= xgrid[Npoints-1])
-    f1 = (x - xgrid[Npoints-2])/h[Npoints-2]
+    cond1 = (x >= xgrid[Nnodes-2]) & (x <= xgrid[Nnodes-1])
+    f1 = (x - xgrid[Nnodes-2])/h[Nnodes-2]
     N_last = Piecewise( (f1, cond1), (0, True) )
     shape_functions.append(N_last)
     
     return shape_functions
 
 Nelements = 2
-Npoints = Nelements + 1
+Nnodes = Nelements + 1
+NnodesPerElement = 2 # linear element
 
 L = Symbol("L", real=True, positive=True)
 k = Symbol("k", real=True, positive=True)
 Q = Symbol("Q", real=True, positive=True)
 
-#L = 1
-xgrid = [ i*L/(Npoints-1) for i in range(Npoints) ] #[0, L/2, L]
-h = [ (xgrid[i+1] - xgrid[i]) for i in range(Npoints-1) ]
-print(xgrid)
-print(h)
+xgrid = [ i*L/(Nnodes-1) for i in range(Nnodes) ]
+h = [ (xgrid[i+1] - xgrid[i]) for i in range(Nnodes-1) ]
 
 x = Symbol("x", positive=True, real=True)
 N = build_shape_functions( x, xgrid, h )
 
-T = Function("T")(x)
-W = Function("W")(x)
-
-LHS = -k * Derivative(T,x,2) - Q
-
+# Nodal values of
 T_i = [ Symbol("T_1", real=True), Symbol("T_2", real=True),  Symbol("T_3", real=True) ]
 expr1 = T_i[0] * N[0] + T_i[1] * N[1]
 
-#pprint( LHS.subs({T: expr1}) )
-#pprint( LHS*N[0] )
-
-#pprint( LHS.subs)
-
 ss = 0
-for j in range(Npoints):
+for j in range(Nnodes):
     ss = ss + diff(N[j], x) * T_i[j]
 
 print("Coeffs:")
-for i in range(Npoints):
+for i in range(NnodesPerElement):
     expr1 = diff(N[i],x) * ss * k
-    expr2 = integrate(expr1, (x,0,L))
-    pprint( expr2.expand().factor() )
+    expr2 = integrate( expr1, (x,0,L/2) )
+    pprint( expr2.expand().factor() ) # Trying to get a suitable form
 
-pprint( integrate( N[0]*Q, (x,0,L/2) ) )
+print("Q contrib")
+for i in range(NnodesPerElement):
+    expr1 = integrate( Q*N[i], (x,0,L/2) )
+    pprint( expr1 )
--- TF4062_KompRekLanjut/FEM/test_05.py
+++ TF4062_KompRekLanjut/FEM/test_05.py
@@ -9,7 +9,7 @@ init_printing()
 # xgrid, h: array (numeric)
 # L: numeric
 def build_shape_functions( x, xgrid, h ):
-    Npoints = len(xgrid)
+    Nnodes = len(xgrid)
     
     # First shape functions
     cond1 = (x >= xgrid[0]) & (x <= xgrid[1])
@@ -18,7 +18,7 @@ def build_shape_functions( x, xgrid, h ):
     
     shape_functions = [ N_first ]
     
-    for i in range(1,Npoints-1):
+    for i in range(1,Nnodes-1):
         #
         cond1 = (x >= xgrid[i-1]) & (x <= xgrid[i])
         f1 = ( x - xgrid[i-1])/h[i-1]
@@ -29,50 +29,40 @@ def build_shape_functions( x, xgrid, h ):
         #
         shape_functions.append(N_i)
     
-    cond1 = (x >= xgrid[Npoints-2]) & (x <= xgrid[Npoints-1])
-    f1 = (x - xgrid[Npoints-2])/h[Npoints-2]
+    cond1 = (x >= xgrid[Nnodes-2]) & (x <= xgrid[Nnodes-1])
+    f1 = (x - xgrid[Nnodes-2])/h[Nnodes-2]
     N_last = Piecewise( (f1, cond1), (0, True) )
     shape_functions.append(N_last)
     
     return shape_functions
 
-Nelements = 2
-Npoints = Nelements + 1
+Nelements = 4
+Nnodes = Nelements + 1
 
 L = Symbol("L", real=True, positive=True)
 k = Symbol("k", real=True, positive=True)
 Q = Symbol("Q", real=True, positive=True)
 
-#L = 1
-xgrid = [ i*L/(Npoints-1) for i in range(Npoints) ] #[0, L/2, L]
-h = [ (xgrid[i+1] - xgrid[i]) for i in range(Npoints-1) ]
+xgrid = [ i*L/(Nnodes-1) for i in range(Nnodes) ]
+h = [ (xgrid[i+1] - xgrid[i]) for i in range(Nnodes-1) ]
 print(xgrid)
 print(h)
 
 x = Symbol("x", positive=True, real=True)
 N = build_shape_functions( x, xgrid, h )
 
-T = Function("T")(x)
-W = Function("W")(x)
+# Nodal values
+#T_i = [ Symbol("T_1", real=True), Symbol("T_2", real=True),  Symbol("T_3", real=True) ]
 
-LHS = -k * Derivative(T,x,2) - Q
-
-T_i = [ Symbol("T_1", real=True), Symbol("T_2", real=True),  Symbol("T_3", real=True) ]
-expr1 = T_i[0] * N[0] + T_i[1] * N[1]
-
-#pprint( LHS.subs({T: expr1}) )
-#pprint( LHS*N[0] )
-
-#pprint( LHS.subs)
+T = [ Symbol("T_"+str(i+1), real=True) for i in range(Nnodes) ]
+pprint(T)
 
 ss = 0
-for j in range(Npoints):
-    ss = ss + diff(N[j], x) * T_i[j]
+for j in range(Nnodes):
+    ss = ss + diff( N[j], x ) * T[j]
 
 print("Coeffs:")
-for i in range(Npoints):
-    expr1 = diff(N[i],x) * ss * k
+for i in range(Nnodes):
+    expr1 = k * diff( N[i], x ) * ss
     expr2 = integrate(expr1, (x,0,L))
     pprint( expr2.expand().factor() )
-
-pprint( integrate( N[0]*Q, (x,0,L/2) ) )
