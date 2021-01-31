c9c0229ca7c846b86e3ec899feb4a91f74024ed7
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-01-31 17:42:22 +0700

    Test float32 version, test using 10 significant digit

--- chapra_7th/ch03/chapra_example_3_2_float32.py
+++ chapra_7th/ch03/chapra_example_3_2_float32.py
@@ -1,22 +1,23 @@
-from math import factorial, exp
+from math import factorial
+import numpy as np
 
 def approx_exp(x, N):
     assert(N >= 0)
     if N == 0:
         return 1
-    s = 0.0
+    s = np.float32(0.0)
     for i in range(N+1):
-        s = s + x**i/factorial(i)
+        s = s + np.float32(x**i)/np.float32(factorial(i))
     return s
 
-x = 0.5
-true_val = exp(x) # from math module
+x = np.float32(0.5)
+true_val = np.exp(x) # from math module
 
 n_digit = 10
 # Equation 3.7
-ε_s_percent = 0.5*10**(2-n_digit)
+ε_s_percent = np.float32(0.5)*np.float32(10**(2-n_digit))
 
-prev_approx = 0.0
+prev_approx = np.float32(0.0)
 for N in range(50):
     approx_val = approx_exp(x, N)
     ε_t_percent = abs(approx_val - true_val)/true_val * 100
@@ -32,3 +33,8 @@ for N in range(50):
 
 print("true_val   is %18.10f" % true_val)
 print("approx_val is %18.10f" % approx_val)
+
+# Make sure that float32 is used
+print()
+print("type(true_val)   = ", type(true_val))
+print("type(approx_val) = ", type(approx_val))
