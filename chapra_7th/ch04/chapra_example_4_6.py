438d8fd2888fc603b57fc6b0a89a23ec7dab463d
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-02-02 18:09:51 +0700

    Example 4.6 using SymPy

--- chapra_7th/ch04/chapra_example_4_6.py
+++ chapra_7th/ch04/chapra_example_4_6.py
@@ -1,10 +1,19 @@
-from uncertainties import ufloat
-from uncertainties.umath import *
+from sympy import *
 
-F = ufloat(750.0, 30) # N/m
-L = ufloat(9, 0.03) # m
-E = ufloat(7.5e9, 5e7) # N/m^2
-I = ufloat(0.0005, 0.000005) # m^4
+F, L, E, I = symbols("F L E I")
+y = F*L**4/(8*E*I)
 
-y = F*L*L*L*L/(8*E*I)
-print("y = ", y)
+F_num = 750; ΔF = 30 # N/m
+L_num = 9; ΔL = 0.03 # m
+E_num = 7.5e9; ΔE = 5e7 # N/m^2
+I_num = 0.0005; ΔI = 0.000005 # m^4
+
+dict_subs = {F: F_num, L: L_num, E: E_num, I: I_num}
+y_num = y.subs(dict_subs)
+print("y = ", y_num)
+
+Δy = abs(diff(y,F))*ΔF + abs(diff(y,L))*ΔL + abs(diff(y,E))*ΔE + abs(diff(y,I))*ΔI
+
+pprint(Δy)
+print("Δy = ", Δy.subs(dict_subs))
+
