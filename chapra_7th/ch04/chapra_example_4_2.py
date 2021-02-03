56ccfd2c95421a234aae6c7d3f601a6bb23a3be3
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-02-03 21:51:00 +0700

    Example 4.2

---
+++ chapra_7th/ch04/chapra_example_4_2.py
@@ -0,0 +1,23 @@
+from sympy import *
+init_printing(use_unicode=True)
+
+x = symbols("x")
+
+f = cos(x)
+
+xi = pi/4
+xip1 = pi/3
+h = xip1 - xi
+
+# zeroth order
+f_approx = cos(xi)
+
+for n in range(1,7): # from 1 to 6
+    new_term = diff(f, x, n) * h**n / factorial(n)
+    f_approx = f_approx + new_term
+    pprint(f_approx)
+    print(N(f_approx.subs({x: xi}))) # use N to force numerical expression
+
+f_true = N(f.subs({x: xip1}))
+print("f_true = ", f_true)
+print(type(f_true))
