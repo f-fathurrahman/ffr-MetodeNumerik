62625eeccae04076628b78365182e4634e9fee9c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-02-11 13:47:11 +0700

    Exercise 6.1

--- chapra_7th/ch06/chapra_exercise_6_1.py
+++ chapra_7th/ch06/chapra_exercise_6_1.py
@@ -1,26 +1,18 @@
-# Simple fixed-point iteration
-
-# f(x) = exp(-x) - x
-# The equation is transformed to
-# x = exp(-x) = g(x)
-#
-# x_{i+1} = exp(-x_{i})
-
-import numpy as np
+from math import sin, sqrt
 
 def f(x):
-    return np.exp(-x) - x
+    return sin(sqrt(x)) - x
 
+# x = sin(sqrt(x))
 def g(x):
-    return np.exp(-x)
-
+    return sin(sqrt(x))
 
 def root_fixed_point(g, x0, NiterMax=100, TOL=1e-10):
     # Initial guess
     x = x0
     for i in range(1,NiterMax+1):
         xnew = g(x)
-        Δx = np.abs(xnew - x)
+        Δx = abs(xnew - x)
         print("%3d %18.10f %13.5e" % (i, xnew, Δx))
         # we are not using relative error here
         if Δx < TOL:
@@ -29,5 +21,5 @@ def root_fixed_point(g, x0, NiterMax=100, TOL=1e-10):
         x = xnew
     return x
 
-xroot = root_fixed_point(g, 0.0)
-print("At root: f(x) = ", f(xroot))
+xroot = root_fixed_point(g, 0.5)
+print("At root f(x) = ", f(xroot))
--- chapra_7th/ch06/root_fixed_point.py
+++ chapra_7th/ch06/root_fixed_point.py
@@ -1,26 +1,9 @@
-# Simple fixed-point iteration
-
-# f(x) = exp(-x) - x
-# The equation is transformed to
-# x = exp(-x) = g(x)
-#
-# x_{i+1} = exp(-x_{i})
-
-import numpy as np
-
-def f(x):
-    return np.exp(-x) - x
-
-def g(x):
-    return np.exp(-x)
-
-
 def root_fixed_point(g, x0, NiterMax=100, TOL=1e-10):
     # Initial guess
     x = x0
     for i in range(1,NiterMax+1):
         xnew = g(x)
-        Δx = np.abs(xnew - x)
+        Δx = abs(xnew - x)
         print("%3d %18.10f %13.5e" % (i, xnew, Δx))
         # we are not using relative error here
         if Δx < TOL:
@@ -28,6 +11,3 @@ def root_fixed_point(g, x0, NiterMax=100, TOL=1e-10):
             break
         x = xnew
     return x
-
-xroot = root_fixed_point(g, 0.0)
-print("At root: f(x) = ", f(xroot))
