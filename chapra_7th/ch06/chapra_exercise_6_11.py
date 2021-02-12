78199109d7ed0be2df35326e29dfbd9f40ed8900
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-02-12 10:46:25 +0700

    Exercise 6.11 with newton-raphson

--- chapra_7th/ch06/chapra_exercise_6_11.py
+++ chapra_7th/ch06/chapra_exercise_6_11.py
@@ -1,14 +1,19 @@
 from math import exp
 from root_secant import *
+from root_newton_raphson import *
 
 def f(x):
     return exp(-0.5*x)*(4 - x) - 2
+
+def df(x):
+    return -0.5*(4 - x)*exp(-0.5*x) - exp(-0.5*x)
 
 # Guess
 #x = 2.0
 x = 6.0 # div by zero
 #x = 8.0
 
-xroot = root_secant(f, x, x + 1.0)
+xroot = root_newton_raphson(f, df, x)
+#xroot = root_secant(f, x, x + 1.0)
 #xroot = root_secant_mod(f, x)
 print(f(xroot))
