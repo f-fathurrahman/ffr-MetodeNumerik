78598ea66d377922a35cdac8f7ef9e3569ae4f6b
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-02-12 20:21:54 +0700

    Exercise 5.19

---
+++ chapra_7th/ch05/chapra_exercise_5_19.py
@@ -0,0 +1,14 @@
+from math import pi
+from root_regula_falsi import *
+
+r = 1.0
+ρs = 200.0
+ρw = 1000.0
+
+def f(h):
+    Vw = pi*h**2/3*(3*r - h) # displaced volume of water
+    Vs = 4*pi*r**3/3
+    return ρw*Vw - ρs*Vs
+
+
+xr = root_regula_falsi(f, 0.5, 0.75)
