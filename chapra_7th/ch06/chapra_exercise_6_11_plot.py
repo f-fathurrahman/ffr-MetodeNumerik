b94b0fbd6eab5718b005147fecdcb713adf146b2
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-02-12 10:46:25 +0700

    Exercise 6.11 with newton-raphson

--- chapra_7th/ch06/chapra_exercise_6_11_plot.py
+++ chapra_7th/ch06/chapra_exercise_6_11_plot.py
@@ -4,9 +4,12 @@ import matplotlib.pyplot as plt
 def f(x):
     return np.exp(-0.5*x)*(4 - x) - 2
 
+def df(x):
+    return -0.5*(4 - x)*np.exp(-0.5*x) - np.exp(-0.5*x)
 
 plt.clf()
 x = np.linspace(0.0, 20.0, 1000)
-plt.plot(x, f(x))
+plt.plot(x, f(x), label="f(x)")
+plt.plot(x, df(x), label="df(x)")
 plt.grid(True)
 plt.savefig("IMG_exe_6_11.pdf")
