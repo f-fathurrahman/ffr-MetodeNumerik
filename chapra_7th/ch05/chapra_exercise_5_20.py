5eb1bdfd6c7b7bebdc4749d6cd735c95a58fd409
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-02-13 10:09:22 +0700

    Exercise 5.20, modified regula false

---
+++ chapra_7th/ch05/chapra_exercise_5_20.py
@@ -0,0 +1,27 @@
+from math import pi
+from root_regula_falsi import *
+from root_bisection import *
+from root_regula_falsi_mod import *
+
+r1 = 0.5
+r2 = 1.0
+h = 1.0
+ρf = 200.0
+ρw = 1000.0
+
+
+def calc_V_frustum(r1, r2, h):
+    return pi*h/3*(r1**2 + r2**2 + r1*r2)
+
+# h1/h = (r2-r1)/r
+# -> r = (r2-r1)*h/h1
+
+def f(h1):
+    r = (r2 - r1)*h/h1
+    Vw = calc_V_frustum(r1 + r, r2, h-h1) # displaced volume of water
+    Vf = calc_V_frustum(r1, r2, h) # cone volume
+    return ρw*Vw - ρf*Vf
+
+xr = root_regula_falsi(f, 0.1, 1.0, NiterMax=1000)
+xr = root_regula_falsi_mod(f, 0.1, 1.0, NiterMax=1000)
+xr = root_bisection(f, 0.01, 1.0, NiterMax=1000)
