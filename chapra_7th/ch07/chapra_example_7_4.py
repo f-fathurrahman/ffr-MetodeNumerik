a4e2a447337a397796826f660f05acd126f2d2e4
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-02-14 10:34:11 +0700

    More methods in example 7.4

--- chapra_7th/ch07/chapra_example_7_4.py
+++ chapra_7th/ch07/chapra_example_7_4.py
@@ -7,8 +7,20 @@ def f(x):
 def fprime(x):
     return 1 + math.sin(x)
 
-#sol = optimize.root_scalar(f, x0=0.0, fprime=fprime, method='newton')
-#sol = optimize.root_scalar(f, x0=0.0, x1=1.0, method='secant')
-#sol = optimize.root_scalar(f, bracket=[0.0, 1.0], method='brentq')
-sol = optimize.root_scalar(f, bracket=[0.0, 1.0], method='bisect')
+print("\nUsing Newton-Raphson method")
+sol = optimize.root_scalar(f, x0=0.0, fprime=fprime, method='newton')
+print(sol)
+
+print("\nUsing secant method")
+sol = optimize.root_scalar(f, x0=0.0, x1=1.0, method='secant')
+print(sol)
+
+# Bracketing method
+for method in ["brentq", "brenth", "ridder", "bisect"]:
+    print("\nUsing %s method" % (method))
+    sol = optimize.root_scalar(f, bracket=[0.0, 1.0], method=method)
+    print(sol)
+
+print("\nUsing bisect directly")
+xroot, sol = optimize.bisect(f, a=0.0, b=1.0, full_output=True)
 print(sol)
