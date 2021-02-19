a4fe0ed7c915aca18c1b97f3a5e08f9dba6ad4b6
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-02-20 00:08:50 +0700

    Test Gauss elim in Chapra

---
+++ chapra_7th/ch09/chapra_example_9_5_v2.py
@@ -0,0 +1,22 @@
+import numpy as np
+from linsolve_gauss_elim_v2 import *
+
+# Solve Ax = b
+
+A = np.array([
+    [3.0, -0.1, -0.2],
+    [0.1, 7.0, -0.3],
+    [0.3, -0.2, 10.0]
+])
+
+b = np.array([
+    [7.85],
+    [-19.3],
+    [71.4]
+])
+
+linsolve_gauss_elim(A, b)
+#x = linsolve_gauss_elim(A, b)
+#print("\nSolusi x = ")
+
+#print(x)
