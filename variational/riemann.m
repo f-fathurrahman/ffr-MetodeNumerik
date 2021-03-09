84e606ee39d42c7586f30ce0e9e84b66696f988e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-03-09 08:24:42 +0700

    Fix Riemann

--- variational/riemann.m
+++ variational/riemann.m
@@ -8,7 +8,21 @@ methods (Static, Access = private)
       sy(i) = trapz(x2, F(i,:));
     end;
     v = trapz(x1, sy);
-  end % function
+  end % function trapz2
+
+  function v = trapz3(x1, x2, x3, F)
+    sz = zeros(length(x1), length(x2));
+    for i = 1:length(x1)
+      for j = 1:length(x2)
+        sz(i,j) = trapz(x2, F(i,j,:));
+      end
+    end
+    sy = zeros(size(x1));
+    for i = 1:length(x1)
+      sy(i) = trapz(x2, sz(i,:));
+    end
+    v = trapz(x1, sy);
+  end % function trapz3
 
 end % methods
 
@@ -24,13 +38,32 @@ methods (Static)
       v = trapz(p.x, F);
     case 2
       v = riemann.trapz2(p.x, p.y, F);
+    case 3
+      v = riemann.trapz3(p.x, p.y, p.z, F);
     otherwise
       v = [];
       disp('Error in arguments for trpzd')
     end % case
-  end
+  end % function trpzd
+
+  function v = mean_value(data)
+    p = data.points;
+    F = data.values;
+    n = p.dim;
+    switch n
+    case 1
+      v = mean(F)*p.measure;
+    case 2
+      v = mean(mean(F))*p.measure;
+    case 3
+      v = mean(mean(mean(F)))*p.measure;
+    otherwise
+      v = [];
+      disp('Error in arguments for mean_value')
+    end % case
+  end % function mean_value
 
 end % methods
 
-end % classdef
+end % classdef riemann
 
--- variational/test_riemann_01.m
+++ variational/test_riemann_01.m
@@ -4,7 +4,7 @@ y = 0:0.01:2;
 F = zeros(length(x), length(y));
 for i = 1:length(x)
   for j = 1:length(y)
-    F(i,j) = x(i)^2 + y(i)^2;
+    F(i,j) = x(i)^2 + y(j)^2;
   end
 end
 
@@ -15,5 +15,7 @@ p.measure = 2;
 data.points = p;
 data.values = F;
 
+disp('Integral: ')
 v1 = riemann.trpzd(data)
-disp(v1)
+disp('Mean value:')
+v2 = riemann.mean_value(data)
