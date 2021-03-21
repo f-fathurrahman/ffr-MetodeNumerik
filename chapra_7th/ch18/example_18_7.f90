53aca8136f51f230772fc7251e47a7881936c151
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-03-21 14:01:17 +0700

    Fix some F90 minted problem, add more problems

--- chapra_7th/ch18/example_18_7.f90
+++ chapra_7th/ch18/example_18_7.f90
@@ -1,12 +1,13 @@
-program example
-  implicit none
-  real(8) :: t(5), v(5)
-  real(8) :: tt, vv
+PROGRAM example_18_7
+  IMPLICIT NONE 
+  REAL(8) :: t(5), v(5)
+  REAL(8) :: tt, vv
 
   t = (/ 1.d0, 3.d0, 5.d0, 7.d0, 13.d0 /)
   v = (/ 800.d0, 2310.d0, 3090.d0, 3940.d0, 4755.d0 /)
 
   tt = 10.d0
-  call lagrange_interp( 5, t, v, tt, vv )
-  write(*,*) 'vv = ', vv
-end program
+  CALL lagrange_interp( 5, t, v, tt, vv )
+  WRITE(*,*) 'vv = ', vv
+END PROGRAM 
+
