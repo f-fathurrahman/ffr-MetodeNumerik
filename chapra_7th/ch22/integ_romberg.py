8d28fbdaeb8d0bd4201a059cb637080a670a1b5c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-04-15 14:40:53 +0700

    Implement Romberg integration

---
+++ chapra_7th/ch22/integ_romberg.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def integ_trapz_multiple( f, a, b, N ):
+    x0 = a
+    xN = b
+    h = (b - a)/N
+    ss = 0.0
+    for i in range(1,N):
+        xi = x0 + i*h
+        ss = ss + f(xi)
+    I = h/2 * ( f(x0) + 2*ss + f(xN) )
+    return I
+
+def integ_romberg(f, a, b, es=1e-10):
+    MAXIT = 10 # XXX HARDCODED
+    I = np.zeros( (MAXIT+1,MAXIT+1) )  # XXX HARDCODED
+    n = 1
+    # We start from I[1,1], to follow the book's notation
+    I[1,1] = integ_trapz_multiple(f, a, b, n)
+    iterConv = 0
+    for i in range(1,MAXIT+1):
+        n = 2**i
+        I[i+1,1] = integ_trapz_multiple(f, a, b, n)
+        #
+        for k in range(2,i+2):
+            j = 2 + i - k
+            I[j,k] = ( 4**(k-1)*I[j+1,k-1] - I[j,k-1] )/ (4**(k-1) - 1)
+        #
+        ea = abs( (I[1,i+1] - I[2,i])/I[1,i+1] )*100 # in percent
+        if ea <= es:
+            iterConv = i
+            #print("converged")
+            break
+        iterConv = 1
+    return I[1,i+1]
+ 
+
+
+
+
---
+++ chapra_7th/ch22/test_romberg.py
@@ -0,0 +1,18 @@
+from math import cos, pi
+
+import sympy
+x = sympy.symbols("x")
+func_symb = 6 + 3*sympy.cos(x)
+resExact = sympy.N(sympy.integrate(func_symb, (x, 0, sympy.pi/2)))
+
+from integ_romberg import *
+
+def my_func(x):
+    return 6 + 3*cos(x)
+
+a = 0.0
+b = pi/2
+resN = integ_romberg(my_func, a, b)
+print("resN = %18.10f" % resN)
+print("res  = %18.10f" % resExact)
+print("err  = %18.10e" % abs(resExact-resN))
