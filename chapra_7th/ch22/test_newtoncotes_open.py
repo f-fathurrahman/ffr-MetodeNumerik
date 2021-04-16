0850ace56347d5fc8b7daa48e0d53c954305958c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-04-16 23:27:02 +0700

    Test Newton-Cotes open formula

---
+++ chapra_7th/ch22/test_newtoncotes_open.py
@@ -0,0 +1,21 @@
+from math import cos, pi
+
+import sympy
+x = sympy.symbols("x")
+func_symb = 6 + 3*sympy.cos(x)
+resExact = sympy.N(sympy.integrate(func_symb, (x, 0, sympy.pi/2)))
+
+from integ_routines_open import *
+from integ_routines import *
+
+def my_func(x):
+    return 6 + 3*cos(x)
+
+a = 0.0
+b = pi/2
+resN = apply_quadrature_multi_interval(
+    integ_newtoncotes_open6seg, my_func, a, b, 10
+)
+print("resN = %18.10f" % resN)
+print("res  = %18.10f" % resExact)
+print("err  = %18.10e" % abs(resExact-resN))
