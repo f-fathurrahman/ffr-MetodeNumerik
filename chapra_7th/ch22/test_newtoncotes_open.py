dc5d451878df8ce1a3805e47fa6a9ec3d23c8540
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-04-20 16:57:57 +0700

    Kuliah 20 April 2021

--- chapra_7th/ch22/test_newtoncotes_open.py
+++ chapra_7th/ch22/test_newtoncotes_open.py
@@ -13,9 +13,44 @@ def my_func(x):
 
 a = 0.0
 b = pi/2
+
 resN = apply_quadrature_multi_interval(
     integ_newtoncotes_open6seg, my_func, a, b, 10
 )
+print("\nUsing Newton-Cotes 6-segments 10 intervals")
 print("resN = %18.10f" % resN)
 print("res  = %18.10f" % resExact)
 print("err  = %18.10e" % abs(resExact-resN))
+
+resN = apply_quadrature_multi_interval(
+    integ_trapz, my_func, a, b, 10
+)
+print("\nUsing trapezoid rule 10 intervals")
+print("resN = %18.10f" % resN)
+print("res  = %18.10f" % resExact)
+print("err  = %18.10e" % abs(resExact-resN))
+
+resN = apply_quadrature_multi_interval(
+    integ_simpson13, my_func, a, b, 10
+)
+print("\nUsing integ_simpson13 rule 10 intervals")
+print("resN = %18.10f" % resN)
+print("res  = %18.10f" % resExact)
+print("err  = %18.10e" % abs(resExact-resN))
+
+resN = apply_quadrature_multi_interval(
+    integ_simpson38, my_func, a, b, 10
+)
+print("\nUsing integ_simpson38 10 intervals")
+print("resN = %18.10f" % resN)
+print("res = %18.10f" % resExact)
+print("err = %18.10e" % abs(resExact-resN))
+
+
+resN = apply_quadrature_multi_interval(
+    integ_boole, my_func, a, b, 10
+)
+print("\nUsing integ_boole 10 intervals")
+print("resN = %18.10f" % resN)
+print("res = %18.10f" % resExact)
+print("err = %18.10e" % abs(resExact-resN))
