9d86f1c1defd9da6fe614e8323b06213d1465367
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-02-10 19:49:46 +0700

    Test again root_muller

--- chapra_7th/ch07/root_muller.py
+++ chapra_7th/ch07/root_muller.py
@@ -1,12 +1,12 @@
-from math import sqrt, pow
 import cmath
 
+# We work with complex arithmetic
 def root_muller(f, xr, h=0.5, TOL=1.0e-10, NiterMax=100):
 
-    res = 0.0;
-    a = xr - h
-    b = xr
-    c = xr + h
+    res = 0.0
+    a = complex(xr - h)
+    b = complex(xr)
+    c = complex(xr + h)
 
     for i in range(1,NiterMax+1): 
       
@@ -26,35 +26,26 @@ def root_muller(f, xr, h=0.5, TOL=1.0e-10, NiterMax=100):
         a1 = (((d2 * h1**2) - (d1 * h2**2)) / ((h1 * h2) * (h1 - h2))) 
         a2 = (((d1 * h2) - (d2 * h1)) / ( h1*h2*(h1 - h2) ))
         discr = a1 * a1 - 4 * a0 * a2
-        is_real = discr >= 0.0
-        if is_real:
-            x = ((-2 * a0) / (a1 + sqrt(discr) ))
-            y = ((-2 * a0) / (a1 - sqrt(discr) ))
-        else:
-            x = ((-2 * a0) / (a1 + cmath.sqrt(discr) ))
-            y = ((-2 * a0) / (a1 - cmath.sqrt(discr) ))
-
+        x = -2 * a0 / (a1 + cmath.sqrt(discr))
+        y = -2 * a0 / (a1 - cmath.sqrt(discr))
+        
         # Taking the root which is closer to x2
-        if is_real:
-            if x >= y: 
-                res = x + c
-            else: 
-                res = y + c 
-        else:
-            # FIXME: need to check this!!!
-            if abs(x-c) >= abs(y-c): 
-                res = x + c
-            else: 
-                res = y + c 
+        # FIXME: need to check this!!!
+        dx = x - c
+        dy = y - c
+        if abs(dx) >= abs(y-c): 
+            res = x + c
+        else: 
+            res = y + c 
 
   
-        print("muller: %3d %18.10f %18.10e" % (i, res, abs(f(res))))
+        print("muller: %3d %18.10e" % (i, abs(f(res))))
         if abs(f(res)) <= TOL:
             break
         
         res_old = res
-        a = b; 
-        b = c; 
-        c = res;
+        a = b 
+        b = c 
+        c = res
 
     return res
--- chapra_7th/ch07/test_root_muller.py
+++ chapra_7th/ch07/test_root_muller.py
@@ -1,17 +1,13 @@
-from cmath import sin, cos
 from root_muller import *
 
 def f(x):
     return x**3 - 13*x - 12.0
 
-#x1 = 1.0
-#xroot = root_muller(f, x1)
-
-# Chapra exercise 6.7
-def chapra_exe_6_7(x):
-    return sin(x) + cos(1 + x**2) - 1
-
-x1 = 1.0 # complex root?
-#x1 = 2.0
-#x1 = 10.0
-xr = root_muller(chapra_exe_6_7, x1)
+x1 = 1.1
+xroot = root_muller(f, x1)
+print("xroot = ", xroot)
+if isinstance(xroot, complex):
+    # Check for imaginary part
+    if abs(xroot.imag) < 1e-12:
+        # xroot is real
+        xroot = xroot.real
