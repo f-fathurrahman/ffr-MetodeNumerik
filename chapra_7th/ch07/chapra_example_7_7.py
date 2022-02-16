27397235ae4e94f369e285fac2df9ffb56928047
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-02-16 16:21:19 +0700

    Add comment

--- chapra_7th/ch07/chapra_example_7_7.py
+++ chapra_7th/ch07/chapra_example_7_7.py
@@ -1,4 +1,7 @@
 from numpy.polynomial import Polynomial
 
+# coefficients are a0, a1, a2, ..., aN
+# N is degree of the polinomial
 p = Polynomial([1.25, -3.875, 2.125, 2.75, -3.5, 1.0])
+print("p = ", p)
 print(p.roots())
