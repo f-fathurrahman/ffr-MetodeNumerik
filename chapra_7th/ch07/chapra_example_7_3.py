5337c1d49237c2af4cba75a45779006da2ec1542
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-02-20 21:40:24 +0700

    Fix again for root_bairstow

--- chapra_7th/ch07/chapra_example_7_3.py
+++ chapra_7th/ch07/chapra_example_7_3.py
@@ -3,9 +3,7 @@ from root_bairstow import *
 a = np.array([1.25, -3.875, 2.125, 2.75, -3.5, 1.0])
 
 print("a before: ", a)
-re, im = root_bairstow(a)
+zroots = root_bairstow(a)
 print("a after: ", a)
 
-print("\nroots:")
-print("re = ", re)
-print("im = ", im)
+print("\nzroots: ", zroots)
