ee9f78fad09b8ec2b94a7afcd2ab9f015550c314
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-03-28 11:58:43 +0700

    Change symbol for golden ratio

--- chapra_7th/ch13/golden_section_optim.py
+++ chapra_7th/ch13/golden_section_optim.py
@@ -6,11 +6,11 @@ def golden_section_optim(f, xlow, xhigh, NiterMax=100, TOL=1e-10):
 
     SMALL = np.finfo(np.float64).resolution
 
-    γ = (np.sqrt(5) - 1)/2
+    ϕ = (np.sqrt(5) - 1)/2
     xl = xlow
     xu = xhigh
     iiter = 1
-    d = γ*(xu - xl)
+    d = ϕ*(xu - xl)
     x1 = xl + d
     x2 = xu - d
     f1 = f(x1)
@@ -26,7 +26,7 @@ def golden_section_optim(f, xlow, xhigh, NiterMax=100, TOL=1e-10):
         fx = f2
 
     for iiter in range(NiterMax):
-        d = γ*d
+        d = ϕ*d
         xint = xu - xl
         if f1 > f2:
             xl = x2
@@ -49,7 +49,7 @@ def golden_section_optim(f, xlow, xhigh, NiterMax=100, TOL=1e-10):
             fx = f2
         #
         if abs(xopt) > SMALL:
-            ea = (1 - γ)*abs(xint/xopt)
+            ea = (1 - ϕ)*abs(xint/xopt)
 
         print("%3d %18.10f %18.10f %15.5e %15.5e" % (iiter, xopt, fx, ea, d))
 
