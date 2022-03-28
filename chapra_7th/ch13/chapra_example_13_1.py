9f9449d8429daeba01b67104a04e24d2008c067c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-03-28 14:08:00 +0700

    Rename to optim_*

--- chapra_7th/ch13/chapra_example_13_1.py
+++ chapra_7th/ch13/chapra_example_13_1.py
@@ -1,9 +1,9 @@
 import numpy as np
-from golden_section_optim import *
+from optim_golden_ratio import *
 
 def f(x):
     return 2*np.sin(x) - x**2/10
 
-xopt, fx = golden_section_optim(f, 0.0, 4.0)
+xopt, fx = optim_golden_ratio(f, 0.0, 4.0)
 print("xopt    = ", xopt)
 print("f(xopt) = ", fx)
