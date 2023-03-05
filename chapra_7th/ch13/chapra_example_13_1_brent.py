d8c7e6072ba2df0fb715f891746e606f0fa0ee4d
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-03-06 06:40:52 +0700

    optim_brent now working

--- chapra_7th/ch13/chapra_example_13_1_brent.py
+++ chapra_7th/ch13/chapra_example_13_1_brent.py
@@ -1,12 +1,13 @@
 import numpy as np
 from optim_brent import *
 
-def f(x):
+def my_func(x):
     return 2*np.sin(x) - x**2/10
 
-def mf(x):
-    return -f(x)
+def m_my_func(x):
+    return -my_func(x)
 
-xopt, fx = optim_brent(mf, 0.0, 4.0, TOL=1e-10)
-print("xopt    = ", xopt)
-print("f(xopt) = ", fx)
+xopt, fxopt = optim_brent(m_my_func, 0.0, 4.0, TOL=1e-10)
+print()
+print("xopt    = %18.10f" % xopt)
+print("f(xopt) = %18.10f" % my_func(xopt))
