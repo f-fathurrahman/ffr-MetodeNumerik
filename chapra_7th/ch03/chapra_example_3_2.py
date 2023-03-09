a0d683b9113b579792f786ec0b1ecd17083a6cab
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-03-09 08:48:06 +0700

    Clarifying, using at least

--- chapra_7th/ch03/chapra_example_3_2.py
+++ chapra_7th/ch03/chapra_example_3_2.py
@@ -12,7 +12,7 @@ def approx_exp(x, N):
 x = 0.5
 true_val = exp(x) # from math module
 
-n_digit = 10
+n_digit = 5
 # Equation 3.7
 ε_s_percent = 0.5*10**(2-n_digit)
 
@@ -27,7 +27,7 @@ for N in range(50):
     prev_approx = approx_val
     print("%3d %18.10f %10.5f%% %10.5f%%" % (N+1, approx_val, ε_t_percent, ε_a_percent))
     if ε_a_percent < ε_s_percent:
-        print("Converged within %d significant digits" % n_digit)
+        print("Converged within at least %d significant digits" % n_digit)
         break
 
 print("true_val   is %18.10f" % true_val)
