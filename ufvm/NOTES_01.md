f93b3bf5b6b42cca8914ae7b16b146480c8a82c0
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2025-06-16 19:36:10 +0700

    Test on Matlab windows

---
+++ ufvm/NOTES_01.md
@@ -0,0 +1,13 @@
+TODO: plot the mesh
+
+```
+% Add path first
+% Start the session
+cfdStartSession;
+% Read OpenFoam files
+cfdReadOpenFoamFiles;
+
+% access global variable Region
+global Region;
+% Debug mesh
+```
--- ufvm/setup_ufvm.m
+++ ufvm/setup_ufvm.m
@@ -1,2 +1,15 @@
-addpath(genpath("/home/efefer/WORKS/my_github_repos/ffr-MetodeNumerik/ufvm/uFVM-master/src"))
-addpath(genpath("/home/efefer/WORKS/my_github_repos/ffr-MetodeNumerik/ufvm/uFVM-master/utilities/"))
+% Linux
+%addpath(genpath("/home/efefer/WORKS/my_github_repos/ffr-MetodeNumerik/ufvm/uFVM-master/src"))
+%addpath(genpath("/home/efefer/WORKS/my_github_repos/ffr-MetodeNumerik/ufvm/uFVM-master/utilities/"))
+
+% Windows (need to use single quotes)
+%addpath(genpath('C:\Users\Fadjar Fathurrahman\WORKS\my_github_repos\ffr-MetodeNumerik\ufvm\src'))
+%addpath(genpath('C:\Users\Fadjar Fathurrahman\WORKS\my_github_repos\ffr-MetodeNumerik\ufvm\src\utilities'))
+
+addpath(genpath('C:\Users\Fadjar Fathurrahman\WORKS\my_github_repos\ffr-MetodeNumerik\ufvm\src_debug'))
+
+% Need a robust way to join strings
+
+% The following works in Matlab
+%addpath(genpath(pwd + "\src"))
+%addpath(genpath(pwd + "\utilities"))
