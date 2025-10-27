36d19111cc479e07dbaa3d2a4d801d1991a21c75
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2025-10-27 07:10:20 +0700

    Test

--- kuzuoglu_fem_elmag/FEM2D/FEM2D_cap.m
+++ kuzuoglu_fem_elmag/FEM2D/FEM2D_cap.m
@@ -3,31 +3,8 @@
 %
 % It solves the 2D electrostatic problem with linear triangular or 
 % bilinear quadrilateral elements.
-%
-%
-% Copyright (C) 2018, Ozlem Ozgun, Mustafa Kuzuoglu
-%
-% This program is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
 
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-%
-% You should have received a copy of the GNU General Public License
-% along with this program.  If not, see <http://www.gnu.org/licenses/>.
-%    
-% Contact:
-% Dr. Ozlem Ozgun
-% email: ozgunozlem@gmail.com
-% web: http://www.ee.hacettepe.edu.tr/~ozlem/
 
-%% ***********************************************************************
-% PRE-PROCESSING
-%*************************************************************************
 clear all; close all;
 tic
 
@@ -37,11 +14,11 @@ elm_type = 1; % 1: triangular, 2: quadrilateral
 
 % Mesh Generation
 %****************
-Lx = .05; % m, length of the computational domain along x axis
-Ly = .05; % m, length of the computational domain along y axis
+Lx = 0.05; % m, length of the computational domain along x axis
+Ly = 0.05; % m, length of the computational domain along y axis
 
-dx = .02; % m, length of the capacitor plate along x axis
-dy = .01; % m, length of the dielectric region inside capacitor along y axis
+dx = 0.02; % m, length of the capacitor plate along x axis
+dy = 0.01; % m, length of the dielectric region inside capacitor along y axis
 
 delx = 0.0005; % m, increment along x axis
 dely = delx;   % m, increment along y axis
--- kuzuoglu_fem_elmag/FEM2D/meshgen_cap.m
+++ kuzuoglu_fem_elmag/FEM2D/meshgen_cap.m
@@ -22,25 +22,6 @@ function [conn,x,y,xmid,ymid,outb_node,ptop_node,pbottom_node,d_elm] = meshgen_c
 % Note: M is the number of elements, N is the number of nodes
 %
 %
-% Copyright (C) 2018, Ozlem Ozgun, Mustafa Kuzuoglu
-%
-% This program is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-%
-% You should have received a copy of the GNU General Public License
-% along with this program.  If not, see <http://www.gnu.org/licenses/>.
-%    
-% Contact:
-% Dr. Ozlem Ozgun
-% email: ozgunozlem@gmail.com
-% web: http://www.ee.hacettepe.edu.tr/~ozlem/
 
 % fit to delh incrementation
 Lx = round(Lx/delx)*delx; % length of the computational domain along x axis
