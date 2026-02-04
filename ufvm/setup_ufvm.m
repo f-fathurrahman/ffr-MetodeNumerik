478c8ca79433735f01d362c2d7c85850083877ea
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2026-02-04 07:29:06 +0700

    Update

--- ufvm/setup_ufvm.m
+++ ufvm/setup_ufvm.m
@@ -3,10 +3,10 @@
 %addpath(genpath("/home/efefer/WORKS/my_github_repos/ffr-MetodeNumerik/ufvm/uFVM-master/utilities/"))
 
 % Windows (need to use single quotes)
-%addpath(genpath('C:\Users\Fadjar Fathurrahman\WORKS\my_github_repos\ffr-MetodeNumerik\ufvm\src'))
-%addpath(genpath('C:\Users\Fadjar Fathurrahman\WORKS\my_github_repos\ffr-MetodeNumerik\ufvm\src\utilities'))
+%addpath(genpath('D:\WORKS\my_github_repos\ffr-MetodeNumerik\ufvm\src'))
+%addpath(genpath('D:\WORKS\my_github_repos\ffr-MetodeNumerik\ufvm\src\utilities'))
 
-addpath(genpath('C:\Users\Fadjar Fathurrahman\WORKS\my_github_repos\ffr-MetodeNumerik\ufvm\src_debug'))
+addpath(genpath('D:\WORKS\my_github_repos\ffr-MetodeNumerik\ufvm\src_debug'))
 
 % Need a robust way to join strings
 
